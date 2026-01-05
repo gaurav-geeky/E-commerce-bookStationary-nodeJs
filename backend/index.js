ddbc55773b2ebe01af40236f1725378c16776f55
gaurav-geeky <gauravmpreat@gmail.com>
2026-01-05 20:42:05 +0530

    Update pnpm lockfile for frontend

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const paymentroute = require("./routes/payment");
 // Use body-parser middleware for JSON and URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 connectDB(); 
